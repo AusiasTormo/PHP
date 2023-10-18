7a7176c382d2c436ee7d854a03c059738291a5f8
Ausias <autorgi@campusaula.com>
2023-10-18 12:37:55 +0200

    ç´ç

--- php-intro/intro.php
+++ php-intro/intro.php
@@ -1,1 +1,14 @@
-hola
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Uso PHP</title>
+</head>
+<body>
+    <h1>Primer uso de php</h1>
+    <?php 
+    //Comentarios en PHP 
+    ?>
+</body>
+</html>
