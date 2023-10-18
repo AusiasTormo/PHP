410227c4f4d575231f219eb564e47ae9899d9feb
Ausias <autorgi@campusaula.com>
2023-10-18 13:16:24 +0200

    v

--- php-intro/intro.php
+++ php-intro/intro.php
@@ -10,7 +10,31 @@
     <?php 
     //Comentarios en PHP
     echo 'Hola caracola';
-     
+    
+    //Puedes añadir etiquetas como texto
+    echo '<br>';
+    echo '<h1>Linea con h1 desde PHP</h1>';
+    echo '<br>';
     ?>
+    <h2>otro bloque de</h2>
+    <?php
+
+    //Añadimos variables al entorno
+    $nombre='Ausias';
+    $edad='19';
+    
+    //echo del contenido de la variable
+    echo $nombre;
+
+    //concatenacion de variables
+    echo '<h3>Mi nombre es ' . $nombre . '</h3>';
+    
+    //interpolacion de variables y texto
+    echo "<h3> Mi edad es: $edad </h3>"
+
+    
+    
+    ?>
+
 </body>
 </html>
